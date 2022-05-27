67bf7255ea62697336ad66c2d89536fcff51952f
Faturachman Dwi Putro <44432754+faturputro@users.noreply.github.com>
2022-05-27 13:11:04 +0700

    applied base styling and search function

--- src/pages/Transactions.js
+++ src/pages/Transactions.js
@@ -12,7 +12,9 @@ export default class Transactions extends View {
   init() {
     window.addEventListener('DOMContentLoaded', () => {
       console.log('loaded')
-      document.querySelector('#filterInput').addEventListener('input', this.filterItems.bind(this))
+      console.log(this.transactionLists)
+      document.querySelector('#filterInput').addEventListener('input', this.onSearchInput.bind(this))
+      document.querySelector('button').addEventListener('click', this.filterLists.bind(this))
     })
   }
   async render() {
@@ -20,27 +22,41 @@ export default class Transactions extends View {
       <div class="container card">
         <h1 class="my-1 mt-2">Transaction</h1>
         <p class="sub-heading">Here's all of your recent transactions.</p>
-        <input id="filterInput" type="text" class="input-field" placeholder="Search transaction" />
-        <ul>
-          ${
-            this.transactionLists.map((item) => {
-              return `
-                <li class="">${item.item}</li>
-              `
-            })
-          }
+        <div class="w-full flex">
+          <input id="filterInput" type="text" class="input-field mr-1" placeholder="Search transaction" />
+          <button type="button" class="btn btn-md">Search</button>
+        </div>
+        <ul class="transaction-list">
+          ${await this.renderListItems()}
         </ul>
       </div>
     `
   }
-  filterItems(e) {
+  async renderListItems() {
+    return await this.transactionLists.map(({ item, logo, price }) => {
+      return `
+        <li class="flex items-center">
+          <div class="img-rounded--container">
+            <img src="${logo}" alt="${item}" />
+          </div>
+          <p class="">${item}</p>
+        </li>
+      `
+    })
+  }
+  async onSearchInput(e) {
     if (e.target.value !== '') {
       this.searchVal = e.target.value
       const filtered = items.filter((item) => this.searchVal.toLowerCase().includes(item.item.toLowerCase()))
       this.transactionLists = filtered
-      this.render()
-      console.log(filtered)
+    } else {
+      this.transactionLists = items
     }
+    document.querySelector('.transaction-list').innerHTML = await this.renderListItems()
+  }
+  async filterLists() {
+    const filtered = items.filter((item) => this.searchVal.toLowerCase().includes(item.item.toLowerCase()))
+    this.transactionLists = filtered
+    document.querySelector('.transaction-list').innerHTML = await this.renderListItems()
   }
 }
-// module.exports = Transactions
